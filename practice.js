0bdd29bc1531398cbba0cb218322c6ac7bcaf72b
Mohamed Affan <mohamedaffan477@gmail.com>
2023-07-12 12:35:01 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,159 @@
+/*const accountId = 144533
+let accountEmail = "mohamedaffan@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+/*My first javascript code*/
+/*
+ console.log(accountId);
+ console.log(accountEmail);
+ console.table([accountEmail,accountId,accountPassword]) 
+ */
+
+//  "use strict";
+
+ //alert(3+3) we are using nodejs not browser
+ //console.log(3+3);
+
+//number - 2 to power 53
+//bigint
+//string - ""
+//null - standalone value
+//undefined - 
+//symbol - unique
+/*
+console.log(typeof "Affan") 
+console.log(typeof 18)
+console.log(typeof null)
+*/
+/*
+let isLoggedIn = true
+console.log(typeof isLoggedIn);
+let isNotLogged = Number(isLoggedIn)
+console.log(typeof isNotLogged);
+console.log(isNotLogged);
+
+let stringValue = "Affan"
+console.log(typeof stringValue);
+let NumberOf = Number(stringValue)
+console.log(typeof NumberOf)
+console.log(NumberOf);
+*/
+
+// let stringVal = "Affan"
+// console.log(typeof stringVal)
+// let NumberOf = Number(stringVal)
+// console.log(typeof NumberOf)
+// console.log(NumberOf);
+
+// let a = 10
+// let b = 20
+// let c = 30 
+// console.log(a+b+c);
+
+// console.log(a*b);
+// console.log(a%b);
+// console.log(b**c);
+// console.log(a/b);
+// console.log(b-c);
+
+// console.log(10 <= 15);
+// console.log(5 == 5);
+// console.log("10" <= 12);
+// console.log("10"=="10");
+// console.log(10==="10");
+
+// let bigValue = 26473254736786947367461874n
+
+// console.log(typeof bigValue);
+
+// let array = ["Affan","Adnan"]
+// console.log(typeof array);
+
+// let newFunction = function(){
+//     console.log("This is the first funcion created by me in javascript");
+// }
+
+// // newFunction();
+// console.log(typeof newFunction);
+
+// let firstObject = {
+//     name: "Affan",
+//     age: 20,
+//     qualification: "BE CSE",
+// }
+
+// console.log(firstObject);
+// console.log(typeof firstObject);
+
+// Stack
+// let first = "Affan"
+// let second = first
+// second = "Adnan"
+
+// console.log(first);
+// console.log(second);
+
+// Heap
+// let first ={
+//     name:"Affan"
+// }
+// let second = first
+
+// second.name = "Irbaz"
+
+// console.log(first.name);
+// console.log(second.name);
+
+// let string = "Affan"
+// console.log(string.length);
+
+// console.log(string.toUpperCase());
+// console.log(string.toLowerCase());
+// console.log(string.charAt(4));
+
+// let stringName = new String("Myself-Affan")
+// console.log(stringName);
+// console.log(stringName.substring(0,4));
+// console.log(stringName.split('-'));
+
+// let name = "Affan"
+// console.log(`My name is ${name}`);
+
+// Number
+
+// const num = 1000
+// const num1 = new Number(1000)
+// console.log(num);
+// console.log(num1);
+// console.log(num.toString());
+// console.log(typeof num);
+// console.log(num.toLocaleString('en-IN'));
+// console.log(num1.valueOf());
+
+//math
+
+// console.log((Math.random()*10)+1);
+
+// const min = 10
+// const max = 20
+// console.log(Math.round((Math.random()*(min-max)+1)+min));
+
+// const num = -50
+// console.log(Math.abs(num));
+
+//Dates
+
+// let date = new Date()
+// console.log(date);
+// console.log(date.toDateString());
+// console.log(date.toISOString());
+// console.log(date.toLocaleString());
+
+// console.log(date.getDate());
+// console.log(date.getFullYear());
+// console.log(date.toLocaleString("default",{
+//     // weekday: "long",
+//     dateStyle: "long"
+// }))
+  
